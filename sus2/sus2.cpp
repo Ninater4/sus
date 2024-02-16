0f96af2700d9c870a3cde96ff63919ea74b6be4b
AkimenkoAV <AkimenkoAV@C7A108-7NEW>
2024-02-16 14:35:26 +0300

    Добавьте файлы проекта.

---
+++ sus2/sus2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    float a, b, c;
+    int num;
+    setlocale(LC_ALL, "RUS");
+    std::cout << "Введите a и b через пробел: ";
+    std::cin >> a >> b;
+    std::cout << "1. Сложение.\n" << "2. Отрицание.\n" << "3. Умножение." << "4. Деление.\n";
+    std::cout << "Введите номер нужного решения:";
+    std::cin >> num;
+    switch (num) {
+        case 1:
+            c = a + b;
+            s
+    }
+}
